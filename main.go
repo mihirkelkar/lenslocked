dc3d045b94fb0bc87082fbd0577d3b4283f91a87
mihirkelkar <kelkarmhr@gmail.com>
2018-12-02 14:42:05 -0800

    adding the signup view

--- main.go
+++ main.go
@@ -12,6 +12,7 @@ import (
 var homeView *views.View
 var contactView *views.View
 var faqView *views.View
+var signupView *views.View
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("content-type", "text/html")
@@ -35,6 +36,13 @@ func faqPage(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func signUp(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-type", "text/html")
+	if err := signupView.Render(w, nil); err != nil {
+		panic(err)
+	}
+}
+
 func errorMessage(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusNotFound)
 	fmt.Fprint(w, "The page you requested could not be found")
@@ -46,11 +54,14 @@ func main() {
 	homeView = views.NewView("bootstrap", "views/home.gohtml")
 	contactView = views.NewView("bootstrap", "views/contact.gohtml")
 	faqView = views.NewView("bootstrap", "views/faq.gohtml")
+	signupView = views.NewView("bootstrap", "views/signup.gohtml")
 
 	r := mux.NewRouter()
 	r.NotFoundHandler = h
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contactUs)
 	r.HandleFunc("/faq", faqPage)
+	r.HandleFunc("/signup", signUp)
+
 	log.Fatal(http.ListenAndServe(":3000", r))
 }
